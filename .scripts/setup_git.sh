9073abc5015b84d7830620a874033b9205e7c427
thyu <tszhoyu@gmail.com>
2017-08-02 13:09:44 -0700

    add setup_git.sh

---
+++ .scripts/setup_git.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git config --global user.name "Tsz-Ho Yu"
+git config --global user.email "thyu@fb.com"
+git config --global core.autocrlf false
+git config --global core.editor "C:/msys64/usr/bin/vim.exe"
+git config --global core.pager "C:/msys64/usr/bin/less.exe"
