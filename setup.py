1142f0ad8901c306267a044c8c20117bbe2d73ae
Tsz Ho Yu <thyu@oculus.com>
2019-04-02 14:08:40 -0700

    Improve Windows support

--- setup.py
+++ setup.py
@@ -14,12 +14,8 @@ import subprocess
 import tempfile
 import shutil
 
-if sys.platform == 'linux' or sys.platform == 'linux2':
-    DOT_DIR = os.path.dirname(os.path.realpath(__file__)).replace('\\','/')
-    HOME_DIR = os.path.realpath(os.path.expanduser('~')).replace('\\','/')
-elif sys.platform == 'win32':
-    DOT_DIR = 'C:/cygwin64/home/thyu/.dotfiles'
-    HOME_DIR = 'C:/cygwin64/home/thyu'
+HOME_DIR = os.path.expanduser('~')
+DOT_DIR = os.path.dirname(os.path.realpath(__file__)).replace('\\','/')
 GIT = 'git'
 LN = 'ln'
 
